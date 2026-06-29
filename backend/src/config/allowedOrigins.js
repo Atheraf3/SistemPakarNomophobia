67a86b974f3f406c69ebaea8c7876fdf6b087252
Muhammad Rafli <r4flimhmmd@gmail.com>
2026-06-29 09:59:34 +0700

    feat: add 'https://www.nomophobiacek.online' to allowed origins

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://sikar-nmp.vercel.app',
     'https://nomophobiacek.online',
+    'https://www.nomophobiacek.online',
     ...(process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',').map(o => o.trim()) : []),
 ];
 
